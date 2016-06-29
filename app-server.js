8958bb049e333f155151f142f2227b2e26a609b3
jesse soldat <jsoldat@hotmail.com>
2016-06-29 20:40:24 +0700

    created APP file using React

--- app-server.js
+++ app-server.js
@@ -2,6 +2,9 @@ var express = require('express');
 
 var app = express();
 
+
+
 app.use(express.static('./public'));
+app.use(express.static('./node_modules/bootstrap/dist'));
 
 var server = app.listen(3000);
---
+++ components/APP.js
@@ -0,0 +1,25 @@
+var React = require('react');
+
+var APP = React.createClass({
+
+	render() {
+		return(
+			<div></div>
+			)
+	}
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = APP;
