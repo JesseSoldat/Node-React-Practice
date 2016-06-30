9c015edbe8547b8f139e050748ebd06b6711619a
jesse soldat <jsoldat@hotmail.com>
2016-06-30 07:50:17 +0700

    when clients connects the socket is pushed to the connections array and on disconnect the socket is removed

--- app-server.js
+++ app-server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 var title = 'Untitled Presentation';
+var connections = [];
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
@@ -12,7 +13,20 @@ var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket) {
+
+
+	socket.once('disconnect', function(){
+		connections.splice(connections.indexOf(socket),1);
+		socket.disconnect();
+
+		console.log('Disconnected:');
+	});
+
 	socket.emit('welcome', {
 		title: title
 	});
+
+	connections.push(socket);
+
+	console.log("Connect: %s sockets connected", connections.length);
 });
